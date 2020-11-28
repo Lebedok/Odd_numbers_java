a920be1d6e84a2ac00a3a9146345065aacea98f9
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-11-27 23:51:59 -0600

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,37 @@
+package HomeWork;
+
+public class OddNumber {
+
+    public static void main(String[] args) {
+
+        int num = 9;
+        boolean oddNumber = num % 2==1;
+
+        System.out.println(num + " is odd number: " + oddNumber);
+
+
+
+        int num2 = 10;
+        boolean oddNumber2 = num2 % 2 ==0 && 0 == 1;
+
+        System.out.println(num2 + " is odd number: " + oddNumber2);
+
+
+
+
+                int number=9;
+                if (number % 2==1);
+                System.out.println(number + "  is odd number " + (number==number));
+
+
+                int numb= 10;
+                if (number % 2==0);
+                System.out.println(numb + "  is odd number " + (numb==number));
+            }
+        }
+
+
+
+
+
+
