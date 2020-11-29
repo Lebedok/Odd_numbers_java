405f05b9adc40f3454c99fae479ac5546d68d19d
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-11-28 22:20:45 -0600

    Update OddNumber.java

--- OddNumber.java
+++ OddNumber.java
@@ -10,25 +10,22 @@ public class OddNumber {
         System.out.println(num + " is odd number: " + oddNumber);
 
 
-
         int num2 = 10;
         boolean oddNumber2 = num2 % 2 ==0 && 0 == 1;
 
         System.out.println(num2 + " is odd number: " + oddNumber2);
 
 
+        int number=9;
+        if (number % 2==1);
+        System.out.println(number + "  is odd number " + (number==number));
 
 
-                int number=9;
-                if (number % 2==1);
-                System.out.println(number + "  is odd number " + (number==number));
-
-
-                int numb= 10;
-                if (number % 2==0);
-                System.out.println(numb + "  is odd number " + (numb==number));
-            }
-        }
+        int numb= 10;
+        if (number % 2==0);
+        System.out.println(numb + "  is odd number " + (numb==number));
+     }
+}
 
 
 
